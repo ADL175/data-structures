8ed225fd1101463633f7994a57c13c9895c2c18f
ADL175 <armydavidlim@gmail.com>
2017-05-23 09:56:31 -0700

    eriks code with tests, tests not working

--- src/linked_list.py
+++ src/linked_list.py
@@ -1,3 +1,5 @@
+import sys
+
 
 class Node(object):
     def __init__(self, value, next_up):
@@ -6,9 +8,11 @@ class Node(object):
 
 
 class Linked_List(object):
-    def __init__(self):
+    def __init__(self, optional_values=[]):
         self.head = None
         self.length = 0
+        for x in optional_values:
+            self.push(x)
 
     def push(self, value):
         self.head = Node(value, self.head)
@@ -19,15 +23,6 @@ class Linked_List(object):
         self.head = self.head.next
         self.length -= 1
         return popped
-
-    def display(self):
-        node = self.head
-        # print(node.data)
-        # while node is not None:
-        #     print(node.data)
-        #     node = node.next
-        node.data = 1
-        print(node.data)
 
     def size(self):
         return self.length
@@ -40,26 +35,61 @@ class Linked_List(object):
                 return "haha, nothing here"
         return temp
 
-    def remove(self, val):
+    def remove_that_nick_does_not_want(self, val):
         if self.head.value is val:
             self.head = self.head.next
+            pass
         current_node = self.head
-        print(current_node.value)
         while current_node:
-            print(current_node.value)
             if current_node.next.value is val:
-                print(current_node.value)
                 self.length -= 1
                 current_node.next = current_node.next.next
+                return
             if current_node is None:
                 return "haha, nothing to delete"
-        print(current_node.value)
-        return current_node
+            current_node = current_node.next
+
+    def remove(self, node_to_be_removed):
+        if self.head is node_to_be_removed:
+            self.head = self.head.next
+            pass
+        current_node = self.head
+        while current_node:
+            if current_node.next is node_to_be_removed:
+                self.length -= 1
+                current_node.next = current_node.next.next
+                break
+            if current_node is None:
+                pass
+            current_node = current_node.next
+
+    def display_nick_doesnt_want_because_I_assumed_we_wanted_nodes_pointing_at_each_other_but_actually_didnt(self):
+        current_node = self.head
+        while current_node.next is not None:
+            print('({})-->'.format(current_node.value)),
+            current_node = current_node.next
+        print('({})'.format(current_node.value))
+
+    def display(self):
+        current_node = self.head
+        the_list = []
+        while current_node:
+            the_list.append(current_node.value)
+            current_node = current_node.next
+        print('('),
+        for i in range(len(the_list)-1):
+            sys.stdout.write('{}, '.format(the_list[i]))
+        sys.stdout.write(str(the_list[-1]))
+        print(')')
 
 
-poop = Linked_List()
-poop.push(3)
-poop.push(5)
-poop.push(4334)
-poop.push(87)
-poop.size()
+new_list = Linked_List()
+new_list.push(1)
+new_list.push(2)
+new_list.push('whisky')
+new_list.push(4)
+to_be_removed = new_list.head
+new_list.push('tango')
+new_list.push(6)
+new_list.push('foxtrot')
+new_list.display()
