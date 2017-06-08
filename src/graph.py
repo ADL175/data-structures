69c037d88aa36751874ebdba21ece836410eaab0
Erik <erik.end@gmail.com>
2017-06-08 13:25:35 -0700

    added weight to graph

--- src/graph.py
+++ src/graph.py
@@ -15,12 +15,13 @@ class Graph(object):
         except KeyError:
             self.graph_dict[value] = []
 
-    def add_edge(self, val1, val2):
+    def add_edge(self, val1, val2, weight=0):
         """Ensure that nodes of val1 and val2 exist (creating them if they don't.
         Then make an edge connecting val1 to val2."""
         self.add_node(val1)
         self.add_node(val2)
-        self.graph_dict[val1].append(val2)
+        self.graph_dict[val1].append([val2, weight])
+        print(self.graph_dict[val1])
 
     def nodes(self):
         """Return a list of all keys in dictionary."""
@@ -34,7 +35,7 @@ class Graph(object):
         to_return = []
         for keys, values in self.graph_dict.items():
             for i in values:
-                to_return.append([keys, i])
+                to_return.append([keys, i[0], i[1]])
         return to_return
 
     def del_node(self, val):
@@ -43,16 +44,20 @@ class Graph(object):
             del self.graph_dict[val]
             for keys, values in self.graph_dict.items():
                 for i in values:
-                    if i == val:
+                    if i[0] == val:
                         values.remove(i)
         except KeyError:
             raise KeyError("No such node exists.")
     def del_edge(self, val1, val2):
         """Deletes an edge from graph."""
         try:
-            self.graph_dict[val1].remove(val2)
+            for node in self.graph_dict[val1]:
+                if node[0] == val2:
+                    self.graph_dict[val1].remove(node)
         except ValueError:
             raise ValueError("No such edge exists.")
+        except KeyError:
+            raise KeyError("No such node exists.")
     def has_node(self, val):
         """Checks if graph has a given node in it."""
         try:
@@ -73,8 +78,9 @@ class Graph(object):
         """Return True if edge exists, else return false."""
         try:
             self.graph_dict[val2]
-            if val2 in self.graph_dict[val1]:
-                return True
+            for node in self.graph_dict[val1]:
+                if node[0] == val2:
+                    return True
             else: 
                 return False
         except KeyError:
@@ -91,7 +97,7 @@ class Graph(object):
                         next_up.push(i)
                 if len(next_up) == 0:
                     break
-                val = next_up.pop().value
+                val = next_up.pop().value[0]
             return seen
         except KeyError:
             raise KeyError('Given value does not exist.')
@@ -107,7 +113,7 @@ class Graph(object):
                         next_up.enqueue(i)
                 if next_up.size() == 0:
                     break
-                val = next_up.dequeue().value
+                val = next_up.dequeue().value[0]
             return seen
         except KeyError:
             raise KeyError('Given value does not exist.')
@@ -164,8 +170,7 @@ if __name__ == '__main__':
     graphy_mcgraphface.add_edge(6, 13)
     graphy_mcgraphface.add_edge(7, 14)
     graphy_mcgraphface.add_edge(7, 15)
-    # print(graphy_mcgraphface.graph_dict)
-    print(graphy_mcgraphface.edges())
+    print(graphy_mcgraphface.graph_dict)
     print('---------------------')
     print('depth: ', graphy_mcgraphface.depth(1))
     print('breadth: ', graphy_mcgraphface.breadth(1))
