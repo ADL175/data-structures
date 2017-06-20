58cd6acfc0636a54a2c4c91687f53b24eeaf1236
ADL175 <armydavidlim@gmail.com>
2017-06-20 15:42:07 -0700

    binary search breadth done

--- src/binary_search.py
+++ src/binary_search.py
@@ -1,9 +1,10 @@
 """Creates a binary tree with search methods."""
 
+
 class Node(object):
 
-    def __init__(self, val, left=None, right=None):
-        self.val = val
+    def __init__(self, value, left=None, right=None):
+        self.value = value
         self.left = left
         self.right = right
 
@@ -19,56 +20,116 @@ class BinarySearchTree(object):
                 for element in iterable:
                     self.insert(element)
 
-    def insert(self, val):
-        if type(val) no in [float, int]:
+    def insert(self, value):
+        if type(value) not in [float, int]:
             raise TypeError('You can only put in numbers.')
 
         # if tree is empty
         if not self._root:
             # new value should be at the tree's root
-            self._root = Node(val)
+            self._root = Node(value)
             # the size of the tree increases by 1
             self._size += 1
 
         else:
             curr = self._root
             while curr:
-                if val > curr.val:  # if new val > root val
+                if value > curr.value:  # if new value > root value
                     if curr.right:  # if right child exists
                         curr = curr.right # move pointer to that child
                     else:
-                        curr.right = Node(val)  # set empty right child of current node to be new node
+                        curr.right = Node(value)  # set empty right child of current node to be new node
                         self._size += 1
                         return
-                elif val < curr.val:  # if new val is less than root val
+                elif value < curr.value:  # if new value is less than root value
                     if curr.left:
                         curr = curr.left
                         continue
                     else:
-                        curr.left = Node(val)
+                        curr.left = Node(value)
                         self._size += 1
                         return
-                else: # if the new val is equal to root value
+                else: # if the new value is equal to root value
                     return
 
+    def insert_recursion(self, value):
+        """This insertion uses a helper function and recursion."""
+        if type(value) not in (float, int):
+            raise TypeError("Numbers only")
 
-    def search(self, val):
-        if type(val) no in [float, int]:
+        self._size += 1
+        self._root = self._insert_helper(self._root, value)
+
+    def _insert_helper(self, root, value):
+        """Helper function for insert_recursion."""
+        if root is None:
+            return Node(value)
+
+        if value < root.value:
+            root.left = self._insert_helper(root.left, value)
+        elif value > root.value:
+            root.right = self._insert_helper(root.right, value)
+        return root
+
+    def search(self, value):
+        if type(value) not in [float, int]:
             raise TypeError('You can only put in numbers.')
 
         curr = self._root
         while curr:
-            if val > curr.val:  # if new val > root val
+            if value > curr.value:  # if new value > root value
                 if curr.right:  # if right child exists
                     curr = curr.right  # move pointer to that child
                     continue
                 else:
                     return
-            elif val < curr.val:  # if new val is less than root val
+            elif value < curr.value:  # if new value is less than root value
                 if curr.left:
                     curr = curr.left
                     continue
                 else:
                     return
-            else: # if the new val is equal to root value
+            else: # if the new value is equal to root value
                 return curr
+
+    def search_recursion(self, value):
+        """This is a search method that uses recursion."""
+        if not self._root:
+            raise IndexError("The tree is empty")
+        if type(value) not in (float, int):
+            raise TypeError("numbers only")
+
+        return self._search_helper(self._root, value)
+
+    def _search_helper(self, root, value):
+        """Helper function for search recursion."""
+        if root.value == value:
+            return root
+        if value < root.value:
+            return self._search_helper(root.left, value)
+        elif value > root.value:
+            return self._search_helper(root.right, value)
+
+    def breadth(self):
+        from que_ import Queue
+        return_list = []
+        next_up = Queue()
+        next_up.enqueue(self._root)
+        while next_up.size() > 0:
+            temp = next_up.dequeue().value
+            if temp.left:
+                next_up.enqueue(temp.left)
+            if temp.right:
+                next_up.enqueue(temp.right)
+            return_list.append(temp.value)
+        for value in return_list:
+            yield value
+
+poo = BinarySearchTree()
+poo.insert(34)
+poo.insert(44)
+poo.insert(14)
+poo.insert(24)
+poo.insert(54)
+poo.insert(11)
+poo.insert(12)
