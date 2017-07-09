079fdc7e431b255c0851938e9befbe24ed6fa8c9
Erik <erik.end@gmail.com>
2017-07-08 22:50:21 -0700

    unweighted

--- src/test_graph.py
+++ src/test_graph.py
@@ -16,11 +16,11 @@ PARAMS_TABLE_HAS_NODE = [
 ]
 
 PARAMS_TABLE_ADD_EDGE = [
-    ([1, 2, 58], [[2, 58]]),
-    (["A", "B", 9], [["B", 9]])
+    ([1, 2], [2]),
+    (["A", "B"], ["B"])
 ]
 PARAMS_TABLE_EDGES = [
-    ([[1, 4, 8], [1, 2, 4], [4, 2, 3]])
+    ([[1, 4], [1, 2], [4, 2]])
 ]
 
 PARAMS_TABLE_DEL_NODE = [
@@ -49,7 +49,7 @@ def test_add_edge(data, result_one):
     test_graph = graph.Graph()
     for val in data:
         test_graph.add_node(val)
-    test_graph.add_edge(data[0], data[1], data[2])
+    test_graph.add_edge(data[0], data[1])
     assert test_graph.graph_dict[data[0]] == result_one
 
 
@@ -68,16 +68,16 @@ def test_edges(data):
     test_graph = graph.Graph()
     for val in data:
         print(val)
-        test_graph.add_edge(val[0], val[1], val[2])
+        test_graph.add_edge(val[0], val[1])
     assert len(test_graph.edges()) == len(data)
 
 
 def test_repeat_edges():
     """Test if error is raised when an edge is added that already exists."""
     test_graph = graph.Graph()
-    test_graph.add_edge(2, 3, 5)
+    test_graph.add_edge(2, 3)
     with pytest.raises(KeyError):
-        test_graph.add_edge(2, 3, 5)
+        test_graph.add_edge(2, 3)
 
 
 @pytest.mark.parametrize("data, delete_me", PARAMS_TABLE_DEL_NODE)
@@ -126,24 +126,24 @@ def test_has_node(data, search, result):
 def test_neighbors():
     """Test if the neighbors method returns list of neighbor."""
     test_graph = graph.Graph()
-    test_graph.add_edge('A', 'C', 1)
-    test_graph.add_edge('A', 'B', -4)
-    test_graph.add_edge('B', 'A', -5)
-    test_graph.add_edge('A', 'D', 6)
-    test_graph.add_edge('C', 'E', 1)
-    test_graph.add_edge('E', 'C', 1)
-    test_graph.add_edge('E', 'B', 1)
-    test_graph.add_edge('B', 'D', 1)
-    test_graph.add_edge('B', 'F', 1)
-    test_graph.add_edge('F', 'D', 10)
-    assert test_graph.neighbors("A") == [['C', 1], ['B', -4], ['D', 6]]
+    test_graph.add_edge('A', 'C')
+    test_graph.add_edge('A', 'B')
+    test_graph.add_edge('B', 'A')
+    test_graph.add_edge('A', 'D')
+    test_graph.add_edge('C', 'E')
+    test_graph.add_edge('E', 'C')
+    test_graph.add_edge('E', 'B')
+    test_graph.add_edge('B', 'D')
+    test_graph.add_edge('B', 'F')
+    test_graph.add_edge('F', 'D')
+    assert test_graph.neighbors("A") == ['C', 'B', 'D']
 
 
 def test_no_neighbors():
     """Test if neighbors function raises error if given a value not in graph."""
     test_graph = graph.Graph()
-    test_graph.add_edge('A', 'C', 1)
-    test_graph.add_edge('A', 'B', -4)
+    test_graph.add_edge('A', 'C')
+    test_graph.add_edge('A', 'B')
     with pytest.raises(KeyError):
         test_graph.neighbors(2)
 
@@ -151,15 +151,15 @@ def test_no_neighbors():
 def test_adjacent():
     """Test adjacent method return boolean and key error for non existent node."""
     test_graph = graph.Graph()
-    test_graph.add_edge('A', 'C', 1)
-    test_graph.add_edge('A', 'B', -4)
-    test_graph.add_edge('B', 'A', -5)
-    test_graph.add_edge('A', 'D', 6)
-    test_graph.add_edge('C', 'E', 1)
-    test_graph.add_edge('E', 'C', 1)
-    test_graph.add_edge('E', 'B', 1)
-    test_graph.add_edge('B', 'F', 1)
-    test_graph.add_edge('F', 'D', 10)
+    test_graph.add_edge('A', 'C')
+    test_graph.add_edge('A', 'B')
+    test_graph.add_edge('B', 'A')
+    test_graph.add_edge('A', 'D')
+    test_graph.add_edge('C', 'E')
+    test_graph.add_edge('E', 'C')
+    test_graph.add_edge('E', 'B')
+    test_graph.add_edge('B', 'F')
+    test_graph.add_edge('F', 'D')
     assert test_graph.adjacent("A", "C") is True
     assert test_graph.adjacent("B", "D") is False
     with pytest.raises(KeyError):
@@ -167,71 +167,3 @@ def test_adjacent():
     with pytest.raises(KeyError):
         test_graph.adjacent("Z", "A")
 
-
-def test_depth_breadth_traversal():
-    """Test both depth and breadth traversal, returns list."""
-    test_graph = graph.Graph()
-    test_graph.add_edge(0, 1, 1)
-    test_graph.add_edge(0, 2, 4)
-    test_graph.add_edge(1, 3, 5)
-    test_graph.add_edge(1, 4, 6)
-    test_graph.add_edge(2, 5, 1)
-    test_graph.add_edge(2, 6, 1)
-    test_graph.add_edge(3, 7, 1)
-    test_graph.add_edge(3, 8, 1)
-    test_graph.add_edge(4, 9, 10)
-    test_graph.add_edge(4, 10, 10)
-    test_graph.add_edge(5, 11, 10)
-    test_graph.add_edge(5, 12, 10)
-    test_graph.add_edge(6, 13, 10)
-    test_graph.add_edge(6, 14, 10)
-    assert test_graph.breadth_first_traversal(0) == [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
-    assert test_graph.depth_first_traversal(0) == [0, 1, 3, 7, 8, 4, 9, 10, 2, 5, 11, 12, 6, 13, 14]
-
-
-def test_depth_breadth_traversal_with_improper_value():
-    """Test both depth and breadth traversal, expecting error if improper starting point."""
-    test_graph = graph.Graph()
-    test_graph.add_edge(0, 1, 1)
-    test_graph.add_edge(0, 2, 4)
-    with pytest.raises(KeyError):
-        test_graph.breadth_first_traversal(5)
-    with pytest.raises(KeyError):
-        test_graph.depth_first_traversal(5)
-
-
-def test_dijkstra_and_bellman_ford():
-    """Test if both algorithms return the correct answer to a shortest path problem."""
-    test_graph = graph.Graph()
-    test_graph.add_edge('A', 'C', 1)
-    test_graph.add_edge('A', 'B', 4)
-    test_graph.add_edge('B', 'A', 5)
-    test_graph.add_edge('A', 'D', 6)
-    test_graph.add_edge('C', 'E', 1)
-    test_graph.add_edge('E', 'C', 1)
-    test_graph.add_edge('E', 'B', 1)
-    test_graph.add_edge('B', 'D', 1)
-    test_graph.add_edge('B', 'F', 1)
-    test_graph.add_edge('F', 'D', 10)
-    assert test_graph.dijkstra('A', 'D') == [4, ['A', 'C', 'E', 'B', 'D']]
-    assert test_graph.bellman_ford('A', 'D') == [4, ['A', 'C', 'E', 'B', 'D']]
-
-
-def test_bellman_ford_negative_weights():
-    """Test if Bellman Ford algorithm raises an error for a negative value loop.
-
-    Not done for Dijkstra, as that is not an explicit part of Dijkstra's algorithm.
-    """
-    test_graph = graph.Graph()
-    test_graph.add_edge('A', 'C', 1)
-    test_graph.add_edge('A', 'B', -4)
-    test_graph.add_edge('B', 'A', -5)
-    test_graph.add_edge('A', 'D', 6)
-    test_graph.add_edge('C', 'E', 1)
-    test_graph.add_edge('E', 'C', 1)
-    test_graph.add_edge('E', 'B', 1)
-    test_graph.add_edge('B', 'D', 1)
-    test_graph.add_edge('B', 'F', 1)
-    test_graph.add_edge('F', 'D', 10)
-    with pytest.raises(ValueError):
-        test_graph.bellman_ford('A', 'D')
